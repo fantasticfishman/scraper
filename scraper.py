5e3d4ade4009152d4dea47003cecfc443f6731be
Justin Bui <40068612+fantasticfishman@users.noreply.github.com>
2022-07-29 00:48:14 -0700

    added user input and persistent folder ability

--- scraper.py
+++ scraper.py
@@ -2,14 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-def imagedown(search):
-    try:
-        os.mkdir(os.path.join(os.getcwd(),search))
-    except:
-        pass
-    os.chdir(os.path.join(os.getcwd(),search))
+
+def imagedown(search,page,index):
     #searchurl = search.replace(' ','+')
-    url = 'https://wallhaven.cc/search?q='+search
+    url = 'https://wallhaven.cc/search?q='+search+'&page='+str(page)
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
 
@@ -21,7 +17,7 @@ def imagedown(search):
         href.append(h)
         print(h)
         
-    count = 0
+    count = index
     for link in href:
         name = str(count)
         imageID = link[23:]
@@ -31,13 +27,88 @@ def imagedown(search):
         check = src
         if(requests.get(check).status_code == 404):
             src = 'https://w.wallhaven.cc/full/'+imageIDshort+'/wallhaven-'+imageID+'.png'
-            with open(search+name+'.png', 'wb') as f:
+            with open(search+' '+name+'.png', 'wb') as f:
                 im = requests.get(src)
                 f.write(im.content)
         else:
-            with open(search+name+'.jpg', 'wb') as f:
+            with open(search+' '+name+'.jpg', 'wb') as f:
                 im = requests.get(src)
                 f.write(im.content)
         count+=1
-        
-imagedown(input('enter your search term baka ') )
+    return count
+
+
+def main():
+    ogdir = os.getcwd()
+    arg = 'cringe'
+    page = 1
+    index = 1
+    while(arg != 'quit'):
+        #get the search term
+        oldarg = arg
+        arg = input('Enter your search term, type continue to go to the next page of your previous search term, or type quit to quit ')
+        #exit
+        if(arg == 'quit'):
+            print('see you next time')
+            exit()
+        elif(arg == 'continue' and oldarg == 'cringe'):
+            print('baka')
+            continue
+            #if continue without selecting search term
+        if(arg == 'continue' and oldarg != 'cringe'):
+            page += 1
+            index = imagedown(oldarg,page,index)
+            print('You are currently on page '+str(page))
+            #continue
+        else:
+            #if no keywords, then search
+            #check if search term has changed, if so make new dir and reset everything
+            if (arg != oldarg):
+                page = 1
+                index = 1
+                if(os.path.exists(os.path.join(ogdir,arg))):
+                    os.chdir(os.path.join(ogdir,arg))
+                    largest = 0
+                    for file in os.listdir(os.path.join(ogdir,arg)):
+                        filename = os.fsdecode(file)
+                        check = int(filename[filename.rfind(' ')+1:-4])
+                        if(check > largest):
+                            largest = check
+                    print(largest)
+                    index = largest+1
+                else:
+                    try:
+                        os.chdir(ogdir)
+                        os.mkdir(os.path.join(ogdir,arg))
+                        os.chdir(os.path.join(ogdir,arg))
+                    except:
+                        pass
+            #get page number
+            try:
+                page = int(input('enter the page number you would like to go to or leave blank for page 1 '))
+                if(os.path.exists(os.path.join(ogdir,arg))):
+                    os.chdir(os.path.join(ogdir,arg))
+                    largest = 0
+                    for file in os.listdir(os.path.join(ogdir,arg)):
+                        filename = os.fsdecode(file)
+                        check = int(filename[filename.rfind(' ')+1:-4])
+                        if(check > largest):
+                            largest = check
+                    print(largest)
+                    index = largest+1
+                else:
+                    try:
+                        os.chdir(ogdir)
+                        os.mkdir(os.path.join(ogdir,arg))
+                        os.chdir(os.path.join(ogdir,arg))
+                    except:
+                        pass
+            except:
+                print('thats not a page baka')
+                continue
+            if(input == ''):
+                page = 1
+            #search for wallpaper and write to current directory, increment index
+            index = imagedown(arg,page,index)
+
+main()
